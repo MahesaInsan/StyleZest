e7769f3a5a39b9fbdf039bc9e34a89d78e31adb0
sachikoyaa <123145737+sachikoyaa@users.noreply.github.com>
2023-05-18 23:47:31 +0700

    Add files via upload

--- app/Http/Controllers/ColorsController.php
+++ app/Http/Controllers/ColorsController.php
@@ -10,6 +10,47 @@ class ColorsController extends Controller
     public function getColors(){
         $colors = color::all();
 
-        return $colors;
+        return view('admin.colorindex', ['colors' => $colors]);
+    }    
+
+    public function addColors(){
+        $colorname = color::all();
+        $colorcode = color::all();
+
+        return view('admin.addcolor', ['colorname' => $colorname, 'colorcode' => $colorcode]);
+    }
+
+    public function saveColors(Request $request){
+        color::insert([
+            'colorname' => $request->colorname,
+            'colorcode' => $request->colorcode
+        ]);
+
+        return redirect()->back()->with('success', 'Color added successfully!');
+    }
+    
+    public function editColors($id){
+        $color = color::find($id);
+        $colorname = color::all();
+        $colorcode = color::all();
+    
+        return view('/admin/editcolors', ['color' => $color, 'colorname' => $colorname, 'colorcode' => $colorcode]);
+    }
+
+    public function updateColors($id, Request $request){
+        $color = color::find($id);
+
+        $color->colorname = $request->colorname;
+        $color->colorcode = $request->colorcode;
+        $color->save();
+
+        return redirect('/admin/color')->with('success', 'Color updated successfully.');
+    }
+
+    public function deleteColors($id){
+        $color = color::findOrFail($id);
+        $color->delete();
+
+        return redirect()->back()->with('success', 'Color deleted successfully');
     }
 }
