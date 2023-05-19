5950813c46e0b1ea2fab9dcfa9c8ae2946c70df7
Mahesa Insan <mahesainsan@gmail.com>
2023-05-19 17:36:02 +0700

    better ui

--- resources/views/buyclothes.blade.php
+++ resources/views/buyclothes.blade.php
@@ -2,34 +2,53 @@
         
 @section('content')
 @csrf
-    <div class="row p-4">
-        <div class="col">
-            <img src="" alt="">
-            <h5>{{$clothes->clothesName}}</h5>
-            <p>{{$clothes->clothesDescription}}</p>
+    <div class="row p-4 h-100">
+        <div class="col h-100">
+            <div class="h-75">
+                <img src="" alt="">
+            </div>
+            <h5 class="fw-bold" style="font-size: 1.25rem">{{$clothes->clothesName}}</h5>
+            <p style="font-size: 1rem">{{$clothes->clothesDescription}}</p>
         </div>
-        <div class="col">
-            <div class="d-flex flex-column">
-                <p>Gender: {{$gender->genderName}}</p>
+        <div class="col h-100">
+            <div class="d-flex flex-column justify-content-between h-100">
+                <div class="d-flex flex-column justify-content-between h-75">
+                    <p style="font-size: 1.25rem">Gender: {{$gender->genderName}}</p>
+                    <div>
+                        <p class="m-0" style="font-size: 1.25rem">Color:</p>
+                        <div class="d-flex flex-row gap-3">
+                            @foreach ($clothes->colors as $color)
+                                <input class="form-check-input" type="radio" name="inColor" value="{{$color->id}}" style="width:3rem; height:3rem; background-color:{{$color->colorcode}};">
+                            @endforeach
+                        </div>
+                    </div>
 
-                <p class="m-0">Color:</p>
-                <div class="d-flex flex-row">
-                    @foreach ($clothes->colors as $color)
-                        <input class="form-check-input" type="radio" name="inColor" value="{{$color->id}}" style="width:2rem; height:2rem; background-color:{{$color->colorcode}}">
-                    @endforeach
+                    <div> 
+                        <p class="m-0" style="font-size: 1.25rem">Size:</p>
+                        <div class="btn-group d-flex flex-row gap-2" style="font-size: 1.25rem">
+                            @foreach ($clothes->sizes as $size)
+                                <label class="btn btn-primary">
+                                    <input class="" type="radio" id="{{$size->sizeCode}}" name="inSize" value="{{$size->id}}"> {{$size->sizeCode}}
+                                </label>
+                            @endforeach
+                        </div>
+                    </div>
+
+                    <div> 
+                        <p class="m-0" style="font-size: 1.25rem">Amount:</p>
+                        <select class="form-select" aria-label="Default select example" name="inAmount" style="font-size: 1.25rem">
+                            @for ($i = 1; $i <= 5; $i++)
+                                <option value={{$i}}>
+                                    {{$i}}
+                                </option>
+                            @endfor
+                        </select>
+                    </div>
+
+                    <h5 class="fw-bold" style="font-size: 1.25rem">Rp. {{$clothes->price}}</h5>
                 </div>
-
-                <p class="m-0">Size:</p>
-                <div class="d-flex flex-row">
-                    @foreach ($clothes->sizes as $size)
-                        {{-- <div class="d-flex align-items-center justify-content-center border border-light" style="background-color:white; width:2rem; height:2rem;">{{$size->sizeCode}}</div> --}}
-                        <input id="selectSize" class="form-check-input d-flex align-items-center justify-content-center border border-light" type="radio" name="inSize" value="{{$size->id}}" style="width:2rem; height:2rem; background-color:white">
-                        <label class="btn btn-outline-primary" for="select-Size">{{$size->sizeCode}}</label>
-                    @endforeach
-                </div>
-
-                <h6>Rp. {{$clothes->price}}</h6>
-                <button>Add to Cart</button>
+                
+                <button class="mb-4">Add to Cart</button>
             </div>
         </div>
     </div>
--- resources/views/layouts/userbox.blade.php
+++ resources/views/layouts/userbox.blade.php
@@ -32,24 +32,28 @@
                 <div class="accordion-item">
                   <h2 class="accordion-header" id="headingTwo">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      Accordion Item #2
+                      Gender
                     </button>
                   </h2>
                   <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                      <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                      @foreach ($genders as $gender)
+                        <li><a href="#">{{$gender->genderName}}</a></li>
+                      @endforeach
                     </div>
                   </div>
                 </div>
                 <div class="accordion-item">
                   <h2 class="accordion-header" id="headingThree">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                      Accordion Item #3
+                      Size
                     </button>
                   </h2>
                   <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                      <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                      @foreach ($sizes as $size)
+                        <li><a href="#">{{$size->sizeCode}}</a></li>
+                      @endforeach
                     </div>
                   </div>
                 </div>
