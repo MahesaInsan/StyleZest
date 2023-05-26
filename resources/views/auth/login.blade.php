a100568f0239615c627b29f5b309e0fd9d4999b9
Mahesa Insan <mahesainsan@gmail.com>
2023-05-26 15:46:50 +0700

    front-end awal website

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,49 +7,53 @@
     @include('cdn')
     <title>StyleZest - Login</title>
 </head>
-<body class="h-100 d-flex justify-content-center align-items-center">
+<body class="h-100 d-flex justify-content-center align-items-center" style="background-image: url('/images/stylezestAssets/bgImage.jpg'); background-repeat: no-repeat; background-size:cover">
     <div class="container h-75">
-        <div class="row justify-content-center align-content-center h-100">
-            <div class="col-md-6 w-50 h-100 d-flex flex-column justify-content-center align-items-center">
-                <div class="card h-100 d-flex flex-column justify-content-center" style="background-color: #3AAFA9;">
-                    <div class="card-body gap-4 d-flex flex-column justify-content-center">
-                        <div class="d-flex flex-column justify-content-center align-items-center fw-bold pb-2" style="font-size:2rem">{{-- {{ __('Login') }} --}}
-                            <img class="w-25 mb-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1024px-Instagram_logo_2022.svg.png" alt="">
-                            <p style="font-size: 1.5rem">WELCOME BACK!</p>
+        <div class="row justify-content-center align-content-center h-100 ">
+            <div class="col-lg-6 col-sm-9 h-100 d-flex flex-column justify-content-center align-items-center">
+                <div class="card h-100 w-100 d-flex flex-column justify-content-center shadow" style="background-color:white;">
+                    <div class="card-body w-100 gap-md-5 gap-sm-3 d-flex flex-column justify-content-center">
+                        <div class="d-flex flex-column justify-content-center align-items-center fw-bold pb-1" style="font-size:2rem">{{-- {{ __('Login') }} --}}
+                            <img class="w-50 mb-2" src="{{url('/images/stylezestAssets/NextEra-Logo.png')}}" alt="" style="object-fit: cover">
+                            <p style="font-size: 1.75rem">Welcome Back!</p>
                         </div>
                         <form class="d-flex flex-column align-items-center justify-content-center w-100" method="POST" action="{{ route('login') }}">
                             @csrf
                             
                             <div class="row mb-3 d-flex justify-content-center w-100" style="font-size: 1.25rem">
-                                <div class="w-50 input-group">
-                                    <span class="input-group-text" id="basic-addon1"><img style="height:20px" src="{{url('/images/stylezestAssets/email-logo.png')}}" alt=""></span>
-                                    <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-    
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                <div class="col-lg-6 col-sm-9">
+                                    <div class="input-group">
+                                        <span class="input-group-text" id="basic-addon1"><img style="height:20px" src="{{url('/images/stylezestAssets/email-logo.png')}}" alt=""></span>
+                                        <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </div>
                             
                             <div class="row mb-3 d-flex justify-content-center w-100" style="font-size: 1.25rem">
-                                <div class="w-50 input-group">
-                                    <span class="input-group-text" id="basic-addon1"><img style="height:20px" src="{{url('/images/stylezestAssets/lock-logo.png')}}" alt=""></span>
-                                    <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-    
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
+                                <div class="col-lg-6 col-sm-9">
+                                    <div class="w-md-50 w-sm-75 input-group">
+                                        <span class="input-group-text" id="basic-addon1"><img style="height:20px" src="{{url('/images/stylezestAssets/lock-logo.png')}}" alt=""></span>
+                                        <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+        
+                                        @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
                                 </div>
                             </div>
                             
                             
                             <div class="row mb-3 w-100 justify-content-center">
-                                <div class="col-xl-6 col-lg-8 col-md-10 col-sm-12">
-                                    <div class="form-check">
+                                <div class="col-lg-6 col-sm-9">
+                                    <div class="form-check" style="font-size:0.95rem">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                         
                                         <label class="form-check-label" for="remember">
@@ -59,21 +63,21 @@
                                 </div>
                             </div>
                             
-                            <div class="row mt-2 mb-0 w-100 justify-content-center">
-                                <div class="d-flex flex-column col-xl-6 col-lg-8 col-md-10 col-sm-12">
+                            <div class="row mt-4 mb-0 w-100 justify-content-center">
+                                <div class="d-flex flex-column col-lg-6 col-sm-9 gap-2">
                                     <div class="" style="font-size: 0.95rem">
-                                        <span>Don't have an account? <a href="{{ route('login') }}" style="color:black">Register</a></span>
+                                        <span>Don't have an account? <a href="{{ route('register') }}" style="color:black">Register</a></span>
                                     </div>
                                     
                                     <button type="submit" class="btn btn-secondary">
                                         {{ __('Login') }}
                                     </button>
                                     
-                                    @if (Route::has('password.request'))
-                                    <a class="btn btn-link d-flex justify-start ps-0" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                    @endif
+{{--                                     @if (Route::has('password.request'))
+                                        <a class="btn btn-link d-flex justify-start ps-0" style="font-size: 0.95rem" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif --}}
                                 </div>
                             </div>
                         </form>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,39 +21,41 @@
             }
         </style>
     </head>
-    <body class="h-100 d-flex justify-content-center align-items-center" style="height: 100vh">
-        <div class="container h-75 bg-gray-100">
+    <body class="h-100 d-flex justify-content-center align-items-center" style="height:100vh; background-image: url('/images/stylezestAssets/bgImage.jpg'); background-repeat: no-repeat; background-size:cover">
+        <div class="container h-75 shadow rounded" style="background-color: white">
             <div class="h-100 col-10 offset-1">
-                <div class="h-100 d-flex flex-column justify-content-center">
+                <div class="h-100 d-flex flex-column justify-content-center gap-4">
                     <div class="row">
                         <div class="col-5 d-flex flex-column justify-content-center align-items-center">
-                            <img src="" alt="">
-                            <h1>Company</h1>
+                            <img class="w-75" src="{{url('/images/stylezestAssets/NextEra-Logo.png')}}" alt="">
+                            <h4>Company</h4>
                         </div>
-                        <div class="col-2 text-center">
+                        <div class="col-2 d-flex justify-content-center align-items-center">
                             <h4>Powered by</h4>
                         </div>
                         <div class="col-5 d-flex flex-column justify-content-center align-items-center">
-                            <img src="" alt="">
-                            <h1>StyleZest</h1>
+                            <img class="w-75" src="{{url('/images/stylezestAssets/StyleZest-Logo.png')}}" alt="">
+                            <h4>StyleZest</h4>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col text-center">
-                            <h4>Presents...</h4>
+                            <h3>Start Your Journey With Us</h3>
                         </div>
                     </div>
     
                     <div>
                         @if (Route::has('login'))
-                            <div class="d-flex">
+                            <div class="d-flex row">
                                 @auth
-                                    <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                                    <a class="col-6 offset-3 btn btn-secondary" href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Continue Browsing</a>
                                 @else
-                                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-            
+                                    <a class="col-3 offset-2 btn btn-secondary" href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                                    <div class="col-2 d-flex justify-content-center align-items-center">Or</div>
+
                                     @if (Route::has('register'))
-                                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                        <a class="col-3 btn btn-secondary" href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                                     @endif
                                 @endauth
                             </div>
